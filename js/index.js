7db6b8803d18223839d9c2846710cbba88465b39
Mariana Trombini <mariana.trombini@dasa.com.br>
2023-04-19 09:11:57 -0300

    :sparkles: feat(ratings): turn input into a select

--- js/index.js
+++ js/index.js
@@ -1,49 +1,43 @@
-// Armazena uma referência à div ultimas-avaliacoes
 const ultimasAvaliacoes = document.querySelector('#last-ratings');
 
-// Função para atualizar a div container teste com as últimas avaliações
 const URL = "http://localhost:3000"
 
 function generateStarRating(score) {
-    const stars = ['<i class="far fa-star"></i>', '<i class="fas fa-star-half-alt"></i>', '<i class="fas fa-star"></i>'];
-    const scoreFloor = Math.floor(score);
-    const scoreDiff = score - scoreFloor;
-    let html = '';
-    for (let i = 0; i < scoreFloor; i++) {
-      html += stars[2];
-    }
-    if (scoreDiff >= 0.5) {
-      html += stars[1];
-    }
-    const remaining = 5 - scoreFloor - (scoreDiff >= 0.5 ? 1 : 0);
-    for (let i = 0; i < remaining; i++) {
-      html += stars[0];
-    }
-    return html;
-  }
-  
+	const stars = ['<i class="far fa-star"></i>', '<i class="fas fa-star-half-alt"></i>', '<i class="fas fa-star"></i>'];
+	const scoreFloor = Math.floor(score);
+	const scoreDiff = score - scoreFloor;
+	let html = '';
+	for (let i = 0; i < scoreFloor; i++) {
+		html += stars[2];
+	}
+	if (scoreDiff >= 0.5) {
+		html += stars[1];
+	}
+	const remaining = 5 - scoreFloor - (scoreDiff >= 0.5 ? 1 : 0);
+	for (let i = 0; i < remaining; i++) {
+		html += stars[0];
+	}
+	return html;
+}
 
 function atualizaAvaliacoes() {
-    // Limpa a div container teste
-    ultimasAvaliacoes.innerHTML = '';
+	ultimasAvaliacoes.innerHTML = '';
 
-    // Faz uma requisição para obter as últimas avaliações
-    fetch(`${URL}/ratings`)
-        .then(response => response.json())
-        .then(ratings => {
-            // Para cada avaliação, cria um novo elemento HTML e adiciona à div ultimas-avaliacoes
-            ratings.forEach(rating => {
-                const date = new Date(rating.createdAt);
-                const options = {
-                    day: "2-digit",
-                    month: "long"
-                };
-                const formattedDate = date.toLocaleDateString("pt-BR", options);
+	fetch(`${URL}/ratings`)
+		.then(response => response.json())
+		.then(ratings => {
+			ratings.forEach(rating => {
+				const date = new Date(rating.createdAt);
+				const options = {
+					day: "2-digit",
+					month: "long"
+				};
+				const formattedDate = date.toLocaleDateString("pt-BR", options);
 
-                const ratingElement = document.createElement("div");
-                ratingElement.classList.add("ratings");
-                ratingElement.innerHTML =
-                    `
+				const ratingElement = document.createElement("div");
+				ratingElement.classList.add("ratings");
+				ratingElement.innerHTML =
+					`
                     <p>${rating.Organizations[0].name}<p>
                     <p>${rating.Organizations[0].amountOfReviews} Avaliações</p>
                     <p>"${rating.title}"</p>
@@ -53,79 +47,142 @@ function atualizaAvaliacoes() {
                     </div>
                     <p>${rating.review}</p>
                 `;
-                ultimasAvaliacoes.appendChild(ratingElement);
-            });
-    
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    }    
+				ultimasAvaliacoes.appendChild(ratingElement);
+			});
 
-// Chama a função para carregar as avaliações iniciais
+		})
+		.catch(error => {
+			console.log(error);
+		});
+}
+
 atualizaAvaliacoes();
-
 
 const form = document.querySelector('form');
 
 const searchOrganization = async (name) => {
-    const response = await fetch(`${URL}/organizations?name=${name}`);
-    const empresas = await response.json();
-    return empresas;
+	const response = await fetch(`${URL}/organizations?name=${name}`);
+	const empresas = await response.json();
+	return empresas;
 };
-
 
 const empresaList = document.getElementById('empresa-list');
 
-document.getElementById('razaoSocialAvaliacao').addEventListener('keyup', async function() {
-    const name = this.value;
-    const empresas = await searchOrganization(name);
-    if (name.length >= 3) {
-        empresaList.innerHTML = '';
-        empresas.forEach((empresa) => {
-            const li = document.createElement('option');
-            li.innerText = empresa.name;
-            li.addEventListener('click', () => {
-                this.value = empresa.name;
-                empresaList.innerHTML = '';
-                this.dataset.organizationId = empresa.id; // Adiciona o id ao dataset do input
-                console.log('this.dataset.OrganizationID', this.dataset.organizationId);
-            });
-            empresaList.appendChild(li);
-        });
-    } else {
-        empresaList.innerHTML = '';
-    }
+document.addEventListener('DOMContentLoaded', () => {
+	const modal = document.querySelector('#avaliar');
+
+	modal.addEventListener('shown.bs.modal', async function() {
+		const name = document.getElementById('empresa-list').value;
+		const empresas = await searchOrganization(name);
+		const empresaList = document.getElementById('empresa-list');
+
+		let selectOrganizationOption = document.createElement("option");
+		empresaList.innerHTML = '';
+		selectOrganizationOption.append('Selecione uma empresa')
+		selectOrganizationOption.setAttribute('disabled', true);
+		selectOrganizationOption.setAttribute('selected', true);
+		empresaList.append(selectOrganizationOption)
+		empresas.forEach((empresa) => {
+			const option = document.createElement('option');
+			option.innerText = empresa.name;
+			option.setAttribute('data-organization-id', empresa.id);
+			option.addEventListener('click', () => {
+				document.getElementById('empresa-list').value = empresa.name;
+				empresaList.innerHTML = '';
+				this.dataset.organizationId = empresa.id
+				document.getElementById('organizationId').value = empresa.id;
+			});
+
+			empresaList.appendChild(option);
+		});
+	});
 });
-// Adiciona um event listener para o botão de enviar a avaliação
+
 form.addEventListener('submit', async (event) => {
-    event.preventDefault();
+	event.preventDefault();
 
-    const personId = "cc765d0e-810d-483d-87c0-274349eb5628";
-    const organizationId = form.elements['razaoSocialAvaliacao'].dataset.organizationId;
-    const title = document.getElementById('tituloAvaliacao').value;
-    const review = document.getElementById('avaliacao').value;
-    const score = document.querySelector('input[name="star"]:checked').value;
-    const dateRate = new Date().toISOString();
+	const title = document.getElementById('tituloAvaliacao').value;
+	const review = document.getElementById('avaliacao').value;
+	const score = document.querySelector('input[name="star"]:checked');
 
-    const response = await fetch(`${URL}/addRating`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            personId,
-            organizationId,
-            title,
-            review,
-            score,
-            dateRate
-        })
-    });
+	if (!title || !review || !score) {
+		const errorMessage = document.createElement('div');
+		errorMessage.textContent = `Todos os campos são obrigatórios. Por favor, verifique o preenchimento e tente novamente.`;
+		errorMessage.classList.add('error-message');
 
-    const rating = await response.json();
-    form.reset()
+		form.insertBefore(errorMessage, form.firstChild);
 
-    // Chama a função para atualizar a div container teste com as novas avaliações
-    atualizaAvaliacoes();
+		return;
+	}
+	const personId = "cc765d0e-810d-483d-87c0-274349eb5628";
+	const organizationId = form.elements['empresa-list'].options[form.elements['empresa-list'].selectedIndex].getAttribute('data-organization-id');
+
+	const dateRate = new Date().toISOString();
+
+	const response = await fetch(`${URL}/addRating`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			personId,
+			organizationId,
+			title,
+			review,
+			score: score.value,
+			dateRate
+		})
+	});
+
+	const rating = await response.json();
+	form.reset()
+
+	const successModal = document.createElement('div');
+	successModal.classList.add('modal', 'fade');
+	successModal.setAttribute('id', 'successModal');
+	successModal.setAttribute('tabindex', '-1');
+	successModal.setAttribute('aria-labelledby', 'successModalLabel');
+	successModal.setAttribute('aria-hidden', 'true');
+	successModal.innerHTML = `
+        <div class="modal-dialog modal-md modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn-close " data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                <p class="sucessfull-sent">Avaliação enviada com sucesso!</p>
+                </div>
+                <div class="modal-footer">
+          <button type="button" class="btn btn-secondary btn-rated btn-size"
+            data-bs-dismiss="modal">Fechar</button>
+        </div>
+            </div>
+        </div>
+    `;
+
+	document.body.appendChild(successModal);
+
+	const modal = new bootstrap.Modal(document.getElementById('successModal'));
+	modal.show();
+
+	const avaliarModal = document.getElementById('avaliar');
+	const bootstrapModal = bootstrap.Modal.getInstance(avaliarModal);
+	bootstrapModal.hide();
+
+	atualizaAvaliacoes();
 });
+
+const removeError = () => {
+	const errorMessage = form.querySelector('.error-message');
+	if (errorMessage) {
+		errorMessage.remove();
+	}
+}
+form.addEventListener('input', () => {
+	removeError()
+});
+
+const resetForm = () => {
+	removeError()
+	form.reset()
+}
