eb0468917fbafb06d06af5d941b2fc8c6c1ee3f3
EnzoHenrico <enzo.mascia1@gmail.com>
2023-05-08 21:04:53 -0300

    interface fixes

--- login/script/index.js
+++ login/script/index.js
@@ -41,5 +41,5 @@ loginForm.addEventListener('submit', function (event) {
 // TODO: colocar o redirecionamento do código para página de cadastro
 const registerButton = document.getElementById('register-button');
 registerButton.addEventListener('click', function () {
-    window.location.href = "../registration/index.html";
+    window.location.href = "../register/index.html";
 });
